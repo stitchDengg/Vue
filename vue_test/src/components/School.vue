56d5e169c83aa0a9db61c97f3650a547e3c3f011
Your Name <1051766345@qq.com>
2022-09-18 17:43:35 +0800

    组件自定义事件

---
+++ vue_test/src/components/School.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="school" >
+    <h2 class="title">学校名称：{{name}}</h2>
+    <h2>学校地址：{{address}}</h2>
+    <button  @click="getName(name)">把学校名称给App</button>
+  </div>
+  
+</template>
+
+<script>
+  export default {
+    name:'School',
+    data() {
+      return {
+        name:'cuc',
+        address:'beijing',
+      }
+    },
+    props:['getSchoolName'],
+    methods:{
+      getName(name){
+        this.getSchoolName(name);
+      }
+    }
+  }
+  
+</script>
+
+<style scoped>
+  .school{
+    background-color: skyblue;
+    padding: 5px;
+    margin-top: 30px;
+  }
+</style>
