dd7289933958a79aa968783723e7bffcf8c72aeb
Your Name <1051766345@qq.com>
2022-09-29 08:21:38 +0800

    插槽

--- vue_test/22_插槽/1_src_默认插槽/App.vue
+++ vue_test/22_插槽/1_src_默认插槽/App.vue
@@ -2,37 +2,19 @@
   <div class="container">
     <CategoryVue title="美食">
       <img
-        slot="center"
         src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fdpic.tiankong.com%2Fvt%2Ff0%2FQJ6859264761.jpg%3Fx-oss-process%3Dstyle%2Fshows&refer=http%3A%2F%2Fdpic.tiankong.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1666960127&t=5e540ea5637a841ec84a25a9f1d931ff"
         alt=""
       />
-      <a slot="footer" href="www.baidu.com">更多美食</a>
     </CategoryVue>
 
     <CategoryVue title="游戏">
-      <ul slot="center">
-        <li v-for="(game, index) in games" :key="index">{{ game }}</li>
+      <ul>
+        <li v-for="(game, index) in games" :key="index">{{game}}</li>
       </ul>
-      <div slot="footer" class="foot">
-        <a href="www.baidu.com">单机游戏</a>
-        <a href="www.baidu.com">网络游戏</a>
-      </div>
     </CategoryVue>
 
     <CategoryVue title="电影">
-      <video
-        slot="center"
-        controls
-        src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"
-      ></video>
-      <template slot="footer">
-        <div class="foot">
-          <a href="www.baidu.com">经典</a>
-          <a href="www.baidu.com">热门</a>
-          <a href="www.baidu.com">推荐</a>
-        </div>
-        <h4>欢迎前来观看影视</h4>
-      </template>
+      <video controls src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"></video>
     </CategoryVue>
   </div>
 </template>
@@ -54,15 +36,9 @@ export default {
 .container {
   display: flex;
   justify-content: space-around;
+  
 }
-video {
+video{
   width: 100%;
-}
-.foot {
-  display: flex;
-  justify-content: space-around;
-}
-h4 {
-  text-align: center;
 }
 </style>
---
+++ vue_test/22_插槽/2_src_具名插槽/components/Category.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="category">
+    <h3>{{title}}</h3>
+    <!-- 定义一个插槽，挖个坑的等着组件的使用者进行填充 -->
+    <slot name="center">我是一些默认值，当使用者没有传进具体结构时，我会出现1</slot>
+    <slot name="footer">我是一些默认值，当使用者没有传进具体结构时，我会出现2</slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'Category',
+    props:['title'],
+  }
+</script>
+
+<style>
+  .category{
+    background-color: skyblue;
+    width: 200px;
+    height: 300px;
+  }
+  h3{
+    text-align: center;
+    background-color: orange;
+  }
+  li{
+    list-style: none;
+  }
+  img{
+    width: 100%;
+  }
+</style>
