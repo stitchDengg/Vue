6147eac14fde303f714d954c27200ab9c03601f5
Your Name <1051766345@qq.com>
2022-10-01 14:05:41 +0800

    mapState和mapGetters

---
+++ vue_test/26_src_求和案例_mapState和mapGetters/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <CountVue></CountVue>
+  </div>
+</template>
+<script>
+import CountVue from './components/Count.vue';
+  export default {
+    components: { CountVue },
+    data() {
+      return {
+
+      };
+    },
+  };
+</script>
+
+<style scoped>
+
+</style>
